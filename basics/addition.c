fbdb355e9ca7f9a87882cbfe58bd19bb3ac72f6f
Abhishek Singh <aksinghpro77@gmail.com>
2023-05-09 14:09:46 +0530

    Addition of 2 numbers

---
+++ basics/addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b = 0;
+    printf("Enter Number 1 ");
+    scanf("%d",&a);
+    printf("Enter numner 2 ");
+    scanf("%d",&b);
+    printf("Addition of two numbers is %d", a+b);
+    return 0;
+}
