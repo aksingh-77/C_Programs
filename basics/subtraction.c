31295adc53c185c583be56850d71f29eae17a011
Abhishek Singh <aksinghpro77@gmail.com>
2023-05-09 14:28:48 +0530

    Substraction of 2 numbers

---
+++ basics/subtraction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    printf("Enter number 1 ");
+    scanf("%d",&a);
+    printf("Enter number 2 ");
+    scanf("%d",&b);
+    printf("Subtraction of 2 numbers is %d", a-b);
+}
