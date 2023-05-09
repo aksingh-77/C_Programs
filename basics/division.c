8257387d7e7c7e38abe93ca2e21011f259b358bc
Abhishek Singh <aksinghpro77@gmail.com>
2023-05-09 16:01:40 +0530

    Multiplication and Division

---
+++ basics/division.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    float a,b;
+    printf("Enter the divisor ");
+    scanf("%f",&a);
+    printf("Enter the dividend ");
+    scanf("%f",&b);
+    printf("Division value is %f", b/a);
+}
---
+++ basics/multiplication.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    printf("Enter numner 1 ");
+    scanf("%d",&a);
+    printf("Enter numner 2 ");
+    scanf("%d",&b);
+    printf("Multiplication of 2 numbers is %d",a*b);
+}
