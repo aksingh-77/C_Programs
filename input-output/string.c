13f2087e4f8ac2ba35f87c44f5920ec66f24ca10
Abhishek Singh <aksinghpro77@gmail.com>
2023-05-13 17:03:57 +0530

    input character

---
+++ input-output/string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    char ch[50];
+    printf("Enter a word: ");
+    scanf("%s",&ch);
+    printf("Entered word is : %s \n",ch);
+
+    // char str[50];
+    // printf("Enter a string with space: ");
+    // scanf("%[^\n]\ns",*str);
+    // //scanf("%[^\n]\ns", str);
+    // printf("Entered String is: %s",str);
+
+}
